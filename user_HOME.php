2073565b2975d42f87632307386781130d53006c
ZH94 <z.hinds94@gmail.com>
2014-12-19 12:20:54 -0800

    Update user_HOME.php
    Corrections made

--- user_HOME.php
+++ user_HOME.php
@@ -2,15 +2,17 @@
 @session_start();
 
 require_once 'user_functions.php';
-// Attempted to login users
-if(!empty($_POST['username']) && !empty($_POST['password'])){
-	$username = $_POST['username'];
-	$pass = $_POST['password']; 
-    $user = new User();
-	$xy = $user->login($username, $pass);
-    if($xy === 1){
-		header('Location: add.php');
+require_once 'msg_func.php';
+
+if(isset($_GET['Inbox'])){
+	header('Location:inbox.php');
 	}
+if(isset($_GET['Sent'])){
+	header('Location:sent.php');
+	}
+if(isset($_GET['logout'])){
+	$user = new User();
+	$xy = $user->logout();
 }
 if(isset($_GET['logout'])){
 	$user = new User();
@@ -33,9 +35,9 @@ if(isset($_GET['logout'])){
 				<div id = "header_sub2" >
 					<div id = "nav_bar">
 						<ul>
-							<li><a href="Admin_HOME.php"> Home </a></li>
-							<li><a href="inbox.php"> Inbox </a></li>
-							<li><a href="sent.php"> Sent </a></li>
+							<li><a href="user_HOME.php"> Home </a></li>
+							<li><a href="inbox2.php"> Inbox </a></li>
+							<li><a href="sent2.php"> Sent </a></li>
 							<li><a href="login.php?logout"> Logout </a></li>
 						</ul>
 					</div>
@@ -52,13 +54,16 @@ if(isset($_GET['logout'])){
 		</div>
 		<div id="C_body">
 			<div id="body_wrapper">
-				<!--sector1 for composing message-->
-				<div id="sector_container">
-					<div class="sector_scheme">
-						<div id = "sector1" >					
-							<div id = "compose">
+				<div id="sub_body">
+					<!--sector1 for composing message-->
+					<div id = "sector1" class="sector_scheme">		
+						<div id = "compose">
+							<div>
 								<h3> Compose New Message </h3>
-								<form method="post" action="" onsubmit="return formvalidation()">
+								<hr>
+							</div>
+							<div>
+								<form id="comp_msg" method="post" action="" onsubmit="return formvalidation()">
 									<div class="msg_field">
 										<label> Subject: </label><br>
 										<input type="text" placeholder="Add a Subject">
@@ -78,91 +83,91 @@ if(isset($_GET['logout'])){
 							</div>
 						</div>
 					</div>
-					<div id="sector_container">
-						<!--sector2 used to display chepo users-->		
-						<div id="sector2" class="sector_scheme" >
-							<h3> Cheapo Users </h3>
-							<div id = "resentmessagaes">
-								<table>
-									<tr>
-										<td><p> cheapo users displayed here using php function to retrieve usenames and emails </p></td>
-									</tr>
-								</table>
+					
+					<!--sector2 used to display chepo users-->		
+					<div id="sector2" class="sector_scheme" >
+							<div>
+								<h3> Cheapo Users </h3>
+								<hr>
+							</div>
+							<div>
+								<div id = "dspusers">
+									<?php
+										$db = new mysqli('localhost','root','', 'cheapousers');
+										$sql = "SELECT id, username FROM users";
+										$result = $db->query($sql);
+										if ($result > 0) {
+											 echo "<table><tr><th>ID</th><th>username</th></tr>";
+											 // output data of each row
+											 while($row = $result->fetch_assoc()) {
+												 echo "<tr><td>" . $row["id"]. "</td><td>" . $row["username"]."</td></tr>";
+											 }
+											 echo "</table>";
+										} else {
+											 echo "0 results";
+										}
+
+										$db->close();
+									?>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div id="sector_container">
-						<!--sector3 used to display last 10 messages-->
-						<div id="sector3" class="sector_scheme" >
+					<!--sector3 used to display last 10 messages-->
+					<div id="sector3" class="sector_scheme" >
+						<div>
 							<h3> Newest Mail </h3>
+							<hr>
+						</div>
+						<div>
 							<table>
-								<div>
-								<tr>  </tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-									<tr> 
-										<td class="user"><a href=" sent mail">user</a></td>
-										<td class="sub"><a href=" sent mail">Subject </a></td>
-										<td class="msg"><a href=" sent mail">Message</a></td>
-									</tr>
-								</div>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
+								<tr> 
+									<td class="user"><a href=" sent mail">user</a></td>
+									<td class="sub"><a href=" sent mail">Subject </a></td>
+								</tr>
 							</table>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div id="C_footer">
-			<div id = "footer_wrapp">
-				<p>Copyright © ermaine Flemmings Roshane Robnson  Zola Hinds</p>
 			</div>
 		</div>
 	</body>
