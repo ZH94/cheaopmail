b3debbcd229dbbac1131f455eb0923b9e6995b04
ZH94 <z.hinds94@gmail.com>
2014-12-19 12:24:25 -0800

    Update inbox2.php

--- inbox2.php
+++ inbox2.php
@@ -33,28 +33,79 @@ if(isset($_GET['logout'])){
 				<div id = "header_sub2" >
 					<div id = "nav_bar">
 						<ul>
-							<li><a href="Admin_HOME.php"> Home </a></li>
-							<li><a href="add.php"> New User </a></li>
-							<li><a href="inbox.php"> Inbox </a></li>
-							<li><a href="sent.php"> Sent </a></li>
+							<li><a href="user_HOME.php"> Home </a></li>
+							<li><a href="inbox2.php"> Inbox </a></li>
+							<li><a href="sent2.php"> Sent </a></li>
 							<li><a href="login.php?logout"> Logout </a></li>
 						</ul>
 					</div>
 				</div>
 				<div id = "header_sub3" >
-					<p>  Hello "User name" </p>
+					<?php
+						$db = new mysqli('localhost','root','', 'cheapousers');
+						$name= $db->query('SELECT username FROM users');
+						$row=$name->fetch_assoc();
+						echo " Hello " .$row["username"];
+					?>
 				</div>
 			</div>
 		</div>
 		<div id="C_body">
 			<div id="body_wrapper">
 				<div id="sub_body">
-					<div id="maillist" class"sector_scheme">
-						jhhggggggggggggggggggggggggggggggggggggggggggggggggggggggggggj
+					<div id="maillist" class="sector_scheme">
+						<div>
+							<div>
+								<h3> Mail </h3>
+								<hr>								
+							</div>
+							<div>
+								<table style="margin:0px auto; margin-top:10px; width:450px;">
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+									<tr>
+										<td>User</td>
+										<td>Subject</td>
+									</tr>
+								</table>
+							</div>
+						</div>
 					</div>
-					</div id="maildisplay" class"sector_scheme">
-						jhhggggggggggggggggggggggggggggggggggggggggggggggggggggggggggj
+					</div id="maildisplay" class="sector_scheme">
+						<div> 
+							<div>
+								<h3> Message </h3>
+								<hr> 
+							</div>
+							<div style="float:left; height:440px; width:480px;">
+								<div>
+									<form style=" margin:0px auto; height:400px; width:460px;">
+										<input style =" marging:0px auto; width:450px; height:320px" type="comment">
+										<input style=" width:100px; height:40px;"type="button" value ="reply">					
+									</form>
+								</div>
+							<div>
+						</div>
 					</div>
+					
 				</div>
 			</div>
 		</div>
